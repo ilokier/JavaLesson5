fd842fb4f985a8a040f44ede9c6de2140603b229
ikierepka <ilona.kierepka@gmail.com>
2022-03-21 09:27:25 +0100

    reformating code

--- src/main/java/Actor.java
+++ src/main/java/Actor.java
@@ -3,21 +3,20 @@ public class Actor {
     private String lastName;
 
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Actor)) return false;
-        Actor actor = (Actor) o;
-        return getName().equals(actor.getName()) && getLastName().equals(actor.getLastName());
-    }
-
-
     public Actor(String name, String lastName) {
         this.name = name;
         this.lastName = lastName;
     }
 
     public Actor() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Actor)) return false;
+        Actor actor = (Actor) o;
+        return getName().equals(actor.getName()) && getLastName().equals(actor.getLastName());
     }
 
     public String getName() {
